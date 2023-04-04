35d30188c6c6f495f687063d9e7d8847dd60efb6
Muhammad Shoaib <muhammad.shoaib@crowdbotics.com>
2023-04-04 14:39:20 +0500

    [Crowdbotics Platform Commit]
    Crowdbotics Studio: Update app screens

--- screens/Untitled15/index.js
+++ screens/Untitled15/index.js
@@ -1,5 +1,6 @@
+import { FlatList } from "react-native";
+import { View } from "react-native";
 import { useSelector } from "react-redux";
-import { Text } from "react-native";
 import { storeproducts_get_products_list } from "../../store/storeProducts/storeproducts_response_get_productlists.slice.js";
 import { useEffect } from "react";
 import { useDispatch } from "react-redux";
@@ -20,7 +21,9 @@ const Untitled15 = () => {
       padding: 10,
       position: 'relative',
       flex: 1
-    }}><Text style={styles.rmqkBToE}>{storeproducts_response_get_products_list.title}</Text></ScrollView>
+    }}><FlatList style={styles.cqmijnCP} renderItem={({
+        item
+      }) => <View style={styles.SsRCIRHY}></View>} ItemSeparatorComponent={() => <View style={styles.OFAoDcNW} />} data={[1, 2, 3]} keyExtractor={(item, index) => index}></FlatList></ScrollView>
     </SafeAreaView>;
 };
 
@@ -34,6 +37,22 @@ const styles = StyleSheet.create({
     lineHeight: 14,
     fontSize: 14,
     borderRadius: 0
+  },
+  cqmijnCP: {
+    position: "absolute",
+    width: "100%",
+    height: "100%",
+    left: 0,
+    top: 0
+  },
+  SsRCIRHY: {
+    width: "100%",
+    height: "100%",
+    backgroundColor: "#FFFFFF"
+  },
+  OFAoDcNW: {
+    backgroundColor: "#000000",
+    height: 1
   }
 });
 export default Untitled15;
