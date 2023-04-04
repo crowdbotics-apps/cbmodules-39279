f07808a8a77925bf8eba7b0b6076f9e21c6fe9c8
Muhammad Shoaib <muhammad.shoaib@crowdbotics.com>
2023-04-04 13:15:17 +0500

    [Crowdbotics Platform Commit]
    Crowdbotics Studio: Update app screens

--- screens/Untitled15/index.js
+++ screens/Untitled15/index.js
@@ -1,7 +1,14 @@
+import { storeproducts_get_products_list } from "../../store/storeProducts/storeproducts_response_get_productlists.slice.js";
+import { useEffect } from "react";
+import { useDispatch } from "react-redux";
 import React from "react";
 import { StyleSheet, ScrollView, SafeAreaView } from "react-native";
 
 const Untitled15 = () => {
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(storeproducts_get_products_list());
+  }, []);
   return <SafeAreaView style={styles.safeArea}>
         <ScrollView contentContainerStyle={{
       backgroundColor: '#f0f0f1',
