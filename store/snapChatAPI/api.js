995fef7994b078d83e7da1c5de36c4d9f17cdd7d
Crowdbotics <engineering@crowdbotics.com>
2023-04-15 10:59:38 +0000

    Update store.

--- store/snapChatAPI/api.js
+++ store/snapChatAPI/api.js
@@ -1,14 +1,21 @@
 import axios from "axios"
-import { SNAPCHAT_API_TOKEN } from "react-native-dotenv"
+import {} from "react-native-dotenv"
 const snapChatAPI = axios.create({
   baseURL: "https://adsapi.snapchat.com",
-  headers: {
-    Accept: "application/json",
-    "Content-Type": "application/json",
-    Authorization: `Bearer ${SNAPCHAT_API_TOKEN}`
-  }
+  headers: { Accept: "application/json", "Content-Type": "application/json" }
 })
 function snapchatapi_get_v1_me_list(payload) {
   return snapChatAPI.get(`/v1/me`)
 }
-export const apiService = { snapchatapi_get_v1_me_list }
+function snapchatapi_post_v1_adaccounts_ad_account_id_campaigns_create(
+  payload
+) {
+  return snapChatAPI.post(
+    `/v1/adaccounts/ad_account_id/campaigns`,
+    payload.data
+  )
+}
+export const apiService = {
+  snapchatapi_get_v1_me_list,
+  snapchatapi_post_v1_adaccounts_ad_account_id_campaigns_create
+}
