c2d79dea6d7758bfb7643667671022cb02f180a2
Crowdbotics <engineering@crowdbotics.com>
2023-04-15 09:05:40 +0000

    Update store.

--- store/mapBoxWebAPIs/api.js
+++ store/mapBoxWebAPIs/api.js
@@ -4,6 +4,19 @@ const mapBoxWebAPIs = axios.create({
   headers: { Accept: "application/json", "Content-Type": "application/json" }
 })
 function mapboxwebapis_get_styles_v1_user_style_id_list(payload) {
-  return mapBoxWebAPIs.get(`/styles/v1/user/style_id`)
+  return mapBoxWebAPIs.get(`/styles/v1/user/style_id`, {
+    params: { access_token: payload.access_token }
+  })
 }
-export const apiService = { mapboxwebapis_get_styles_v1_user_style_id_list }
+function mapboxwebapis_get_directions_v5_mapbox_profile_origin_latorigin_lngdest_latdest_lngjson_list(
+  payload
+) {
+  return mapBoxWebAPIs.get(
+    `/directions/v5/mapbox/profile/origin_lat,origin_lng:dest_lat,dest_lng.json`,
+    { params: { access_token: payload.access_token } }
+  )
+}
+export const apiService = {
+  mapboxwebapis_get_styles_v1_user_style_id_list,
+  mapboxwebapis_get_directions_v5_mapbox_profile_origin_latorigin_lngdest_latdest_lngjson_list
+}
