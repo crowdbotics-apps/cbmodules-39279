3d3487332ee1b7eace75d8591607d2b4b904264d
Crowdbotics <engineering@crowdbotics.com>
2023-04-15 13:06:24 +0000

    Update store.

--- store/emojiIntegration/api.js
+++ store/emojiIntegration/api.js
@@ -8,4 +8,12 @@ function emojiintegration_get_emojis_list(payload) {
     params: { access_key: payload.access_key }
   })
 }
-export const apiService = { emojiintegration_get_emojis_list }
+function emojiintegration_get_emojis_emojislug_list(payload) {
+  return emojiIntegration.get(`/emojis/emoji-slug`, {
+    params: { access_key: payload.access_key }
+  })
+}
+export const apiService = {
+  emojiintegration_get_emojis_list,
+  emojiintegration_get_emojis_emojislug_list
+}
