70e869eb5b20d0f3855ae99dd47611e8249384ed
Crowdbotics <engineering@crowdbotics.com>
2023-04-15 06:13:08 +0000

    Update store.

--- store/calendlyAPI/calendlyapi_response_get_GetCalendlyUsers.slice.js
+++ store/calendlyAPI/calendlyapi_response_get_GetCalendlyUsers.slice.js
@@ -1,26 +1,24 @@
 import { createSlice, createAsyncThunk } from "@reduxjs/toolkit"
 import { apiService } from "./api"
-export const calendlyapisupport_get_users_me_read = createAsyncThunk(
-  "calendlyapisupport_response_get_GetCalendlyUsers/calendlyapisupport_get_users_me_read",
+export const calendlyapi_get_users_me_read = createAsyncThunk(
+  "calendlyapi_response_get_GetCalendlyUsers/calendlyapi_get_users_me_read",
   async payload => {
-    const response = await apiService.calendlyapisupport_get_users_me_read(
-      payload
-    )
+    const response = await apiService.calendlyapi_get_users_me_read(payload)
     return response.data
   }
 )
 const initialState = { entities: [], api: { loading: "idle", error: null } }
-const calendlyapisupport_response_get_GetCalendlyUsersSlice = createSlice({
-  name: "calendlyapisupport_response_get_GetCalendlyUsers",
+const calendlyapi_response_get_GetCalendlyUsersSlice = createSlice({
+  name: "calendlyapi_response_get_GetCalendlyUsers",
   initialState,
   reducers: {},
   extraReducers: {
-    [calendlyapisupport_get_users_me_read.pending]: (state, action) => {
+    [calendlyapi_get_users_me_read.pending]: (state, action) => {
       if (state.api.loading === "idle") {
         state.api.loading = "pending"
       }
     },
-    [calendlyapisupport_get_users_me_read.fulfilled]: (state, action) => {
+    [calendlyapi_get_users_me_read.fulfilled]: (state, action) => {
       if (state.api.loading === "pending") {
         state.entities = [
           ...state.entities.filter(record => record.id !== action.payload.id),
@@ -29,7 +27,7 @@ const calendlyapisupport_response_get_GetCalendlyUsersSlice = createSlice({
         state.api.loading = "idle"
       }
     },
-    [calendlyapisupport_get_users_me_read.rejected]: (state, action) => {
+    [calendlyapi_get_users_me_read.rejected]: (state, action) => {
       if (state.api.loading === "pending") {
         state.api.error = action.error
         state.api.loading = "idle"
@@ -38,6 +36,6 @@ const calendlyapisupport_response_get_GetCalendlyUsersSlice = createSlice({
   }
 })
 export default {
-  calendlyapisupport_get_users_me_read,
-  slice: calendlyapisupport_response_get_GetCalendlyUsersSlice
+  calendlyapi_get_users_me_read,
+  slice: calendlyapi_response_get_GetCalendlyUsersSlice
 }
