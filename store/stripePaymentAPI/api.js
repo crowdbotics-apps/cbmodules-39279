a0f1e59c74e9b8074cd73e4da2fd593de77a0de5
Crowdbotics <engineering@crowdbotics.com>
2023-04-15 10:01:43 +0000

    Update store.

--- store/stripePaymentAPI/api.js
+++ store/stripePaymentAPI/api.js
@@ -1,14 +1,16 @@
 import axios from "axios"
-import { STRIPE_PAYMENT_API_TOKEN } from "react-native-dotenv"
+import {} from "react-native-dotenv"
 const stripePaymentAPI = axios.create({
   baseURL: "https://api.stripe.com",
-  headers: {
-    Accept: "application/json",
-    "Content-Type": "application/json",
-    Authorization: `Bearer ${STRIPE_PAYMENT_API_TOKEN}`
-  }
+  headers: { Accept: "application/json", "Content-Type": "application/json" }
 })
+function stripepaymentapi_get_v1_balance_list(payload) {
+  return stripePaymentAPI.get(`/v1/balance`)
+}
 function stripepaymentapi_post_v1_payment_intents_create(payload) {
   return stripePaymentAPI.post(`/v1/payment_intents`, payload.data)
 }
-export const apiService = { stripepaymentapi_post_v1_payment_intents_create }
+export const apiService = {
+  stripepaymentapi_get_v1_balance_list,
+  stripepaymentapi_post_v1_payment_intents_create
+}
