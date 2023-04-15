9e29082533922414ebdc6859a069bc9b4524ffdc
Crowdbotics <engineering@crowdbotics.com>
2023-04-15 11:54:14 +0000

    Update store.

--- store/zoomAPI/api.js
+++ store/zoomAPI/api.js
@@ -1,20 +1,16 @@
 import axios from "axios"
-import { ZOOM_API_TOKEN } from "react-native-dotenv"
+import {} from "react-native-dotenv"
 const zoomAPI = axios.create({
   baseURL: "https://api.zoom.us/v2",
-  headers: {
-    Accept: "application/json",
-    "Content-Type": "application/json",
-    Authorization: `Bearer ${ZOOM_API_TOKEN}`
-  }
+  headers: { Accept: "application/json", "Content-Type": "application/json" }
 })
 function zoomapi_get_users_list(payload) {
   return zoomAPI.get(`/users/`)
 }
-function zoomapi_get_users_user_Id_meetings_read(payload) {
+function zoomapi_get_users_user_Id_meetings_list(payload) {
   return zoomAPI.get(`/users/${payload.user_Id}/meetings`)
 }
 export const apiService = {
   zoomapi_get_users_list,
-  zoomapi_get_users_user_Id_meetings_read
+  zoomapi_get_users_user_Id_meetings_list
 }
