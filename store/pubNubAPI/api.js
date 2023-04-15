5d2ecc6a1afdad4c54320a50030430dc515c5702
Crowdbotics <engineering@crowdbotics.com>
2023-04-15 11:27:11 +0000

    Update store.

--- store/pubNubAPI/api.js
+++ store/pubNubAPI/api.js
@@ -3,6 +3,9 @@ const pubNubAPI = axios.create({
   baseURL: "https://ps.pndsn.com",
   headers: { Accept: "application/json", "Content-Type": "application/json" }
 })
+function pubnubapi_get_v1_files_sub_key_channels_channel_files_list(payload) {
+  return pubNubAPI.get(`/v1/files/sub_key/channels/channel/files`)
+}
 function pubnubapi_post_publish_pub_key_sub_key_0_channel_callback_create(
   payload
 ) {
@@ -13,5 +16,6 @@ function pubnubapi_post_publish_pub_key_sub_key_0_channel_callback_create(
   )
 }
 export const apiService = {
+  pubnubapi_get_v1_files_sub_key_channels_channel_files_list,
   pubnubapi_post_publish_pub_key_sub_key_0_channel_callback_create
 }
