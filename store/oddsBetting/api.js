f67e30509cbead5874c9a8a65bc623e983d7c70f
Crowdbotics <engineering@crowdbotics.com>
2023-04-27 15:38:40 +0000

    Update store.

---
+++ store/oddsBetting/api.js
@@ -0,0 +1,29 @@
+import axios from "axios"
+const oddsBetting = axios.create({
+  baseURL: "https://api.the-odds-api.com/v4",
+  headers: { Accept: "application/json", "Content-Type": "application/json" }
+})
+function oddsbetting_get_scores_list(payload) {
+  return oddsBetting.get(`/scores/`, {
+    params: { daysFrom: payload.daysFrom, apiKey: payload.apiKey }
+  })
+}
+function oddsbetting_get_sports_read(payload) {
+  return oddsBetting.get(`/sports/`, { params: { apikey: payload.apikey } })
+}
+function oddsbetting_get_sports_americanfootball_nfl_oddshistory_read(payload) {
+  return oddsBetting.get(`/sports/americanfootball_nfl/odds-history/`, {
+    params: {
+      apiKey: payload.apiKey,
+      regions: payload.regions,
+      markets: payload.markets,
+      oddsFormat: payload.oddsFormat,
+      date: payload.date
+    }
+  })
+}
+export const apiService = {
+  oddsbetting_get_scores_list,
+  oddsbetting_get_sports_read,
+  oddsbetting_get_sports_americanfootball_nfl_oddshistory_read
+}
