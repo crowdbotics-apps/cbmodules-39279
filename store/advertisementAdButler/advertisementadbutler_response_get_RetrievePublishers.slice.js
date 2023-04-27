02e3a6bcb1b92fa1ab58fdbfda10a0472e2394e7
Crowdbotics <engineering@crowdbotics.com>
2023-04-27 15:37:33 +0000

    Update store.

--- store/advertisementAdButler/advertisementadbutler_response_get_RetrievePublishers.slice.js
+++ store/advertisementAdButler/advertisementadbutler_response_get_RetrievePublishers.slice.js
@@ -1,41 +1,52 @@
-import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
-import { apiService } from "./api";
-export const advertisementadbutler_get_publishers_publisherid_read = createAsyncThunk("advertisementadbutler_response_get_RetrievePublishers/advertisementadbutler_get_publishers_publisherid_read", async payload => {
-  const response = await apiService.advertisementadbutler_get_publishers_publisherid_read(payload);
-  return response.data;
-});
-const initialState = {
-  entities: [],
-  api: {
-    loading: "idle",
-    error: null
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit"
+import { apiService } from "./api"
+export const advertisementadbutler_get_publishers_publisherid_read = createAsyncThunk(
+  "advertisementadbutler_response_get_RetrievePublishers/advertisementadbutler_get_publishers_publisherid_read",
+  async payload => {
+    const response = await apiService.advertisementadbutler_get_publishers_publisherid_read(
+      payload
+    )
+    return response.data
   }
-};
+)
+const initialState = { entities: [], api: { loading: "idle", error: null } }
 const advertisementadbutler_response_get_RetrievePublishersSlice = createSlice({
   name: "advertisementadbutler_response_get_RetrievePublishers",
   initialState,
   reducers: {},
   extraReducers: {
-    [advertisementadbutler_get_publishers_publisherid_read.pending]: (state, action) => {
+    [advertisementadbutler_get_publishers_publisherid_read.pending]: (
+      state,
+      action
+    ) => {
       if (state.api.loading === "idle") {
-        state.api.loading = "pending";
+        state.api.loading = "pending"
       }
     },
-    [advertisementadbutler_get_publishers_publisherid_read.fulfilled]: (state, action) => {
+    [advertisementadbutler_get_publishers_publisherid_read.fulfilled]: (
+      state,
+      action
+    ) => {
       if (state.api.loading === "pending") {
-        state.entities = [...state.entities.filter(record => record.id !== action.payload.id), action.payload];
-        state.api.loading = "idle";
+        state.entities = [
+          ...state.entities.filter(record => record.id !== action.payload.id),
+          action.payload
+        ]
+        state.api.loading = "idle"
       }
     },
-    [advertisementadbutler_get_publishers_publisherid_read.rejected]: (state, action) => {
+    [advertisementadbutler_get_publishers_publisherid_read.rejected]: (
+      state,
+      action
+    ) => {
       if (state.api.loading === "pending") {
-        state.api.error = action.error;
-        state.api.loading = "idle";
+        state.api.error = action.error
+        state.api.loading = "idle"
       }
     }
   }
-});
+})
 export default {
   advertisementadbutler_get_publishers_publisherid_read,
   slice: advertisementadbutler_response_get_RetrievePublishersSlice
-};
+}
--- store/advertisementAdButler/advertisementadbutler_response_post_CreatePublishers.slice.js
+++ store/advertisementAdButler/advertisementadbutler_response_post_CreatePublishers.slice.js
@@ -1,16 +1,15 @@
-import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
-import { apiService } from "./api";
-export const advertisementadbutler_post_publishers_create = createAsyncThunk("advertisementadbutler_response_post_CreatePublishers/advertisementadbutler_post_publishers_create", async payload => {
-  const response = await apiService.advertisementadbutler_post_publishers_create(payload);
-  return response.data;
-});
-const initialState = {
-  entities: [],
-  api: {
-    loading: "idle",
-    error: null
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit"
+import { apiService } from "./api"
+export const advertisementadbutler_post_publishers_create = createAsyncThunk(
+  "advertisementadbutler_response_post_CreatePublishers/advertisementadbutler_post_publishers_create",
+  async payload => {
+    const response = await apiService.advertisementadbutler_post_publishers_create(
+      payload
+    )
+    return response.data
   }
-};
+)
+const initialState = { entities: [], api: { loading: "idle", error: null } }
 const advertisementadbutler_response_post_CreatePublishersSlice = createSlice({
   name: "advertisementadbutler_response_post_CreatePublishers",
   initialState,
@@ -18,24 +17,30 @@ const advertisementadbutler_response_post_CreatePublishersSlice = createSlice({
   extraReducers: {
     [advertisementadbutler_post_publishers_create.pending]: (state, action) => {
       if (state.api.loading === "idle") {
-        state.api.loading = "pending";
+        state.api.loading = "pending"
       }
     },
-    [advertisementadbutler_post_publishers_create.fulfilled]: (state, action) => {
+    [advertisementadbutler_post_publishers_create.fulfilled]: (
+      state,
+      action
+    ) => {
       if (state.api.loading === "pending") {
-        state.entities.push(action.payload);
-        state.api.loading = "idle";
+        state.entities.push(action.payload)
+        state.api.loading = "idle"
       }
     },
-    [advertisementadbutler_post_publishers_create.rejected]: (state, action) => {
+    [advertisementadbutler_post_publishers_create.rejected]: (
+      state,
+      action
+    ) => {
       if (state.api.loading === "pending") {
-        state.api.error = action.error;
-        state.api.loading = "idle";
+        state.api.error = action.error
+        state.api.loading = "idle"
       }
     }
   }
-});
+})
 export default {
   advertisementadbutler_post_publishers_create,
   slice: advertisementadbutler_response_post_CreatePublishersSlice
-};
+}
