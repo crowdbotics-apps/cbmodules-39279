84ac73db32f6415585f734841fb38795b829c9a2
Saad bin abid <saad.abid@crowdbotics.com>
2023-04-27 13:20:28 +0500

    Moved calendly authorization token to env

--- store/calendlyAPI/api.js
+++ store/calendlyAPI/api.js
@@ -1,23 +1,25 @@
-import axios from "axios";
-import "react-native-dotenv";
+import axios from "axios"
+import "react-native-dotenv"
+import { CALENDLY_TOKEN } from "@env"
+
 const calendlyAPI = axios.create({
   baseURL: "https://api.calendly.com/",
   headers: {
     Accept: "application/json",
     "Content-Type": "application/json",
-    "Authorization": "Bearer eyJraWQiOiIxY2UxZTEzNjE3ZGNmNzY2YjNjZWJjY2Y4ZGM1YmFmYThhNjVlNjg0MDIzZjdjMzJiZTgzNDliMjM4MDEzNWI0IiwidHlwIjoiUEFUIiwiYWxnIjoiRVMyNTYifQ.eyJpc3MiOiJodHRwczovL2F1dGguY2FsZW5kbHkuY29tIiwiaWF0IjoxNjcwOTIyNzkyLCJqdGkiOiI5OTRhMWNiYi01NGI2LTQwMGMtYTZmYy0xYzZiMWFkNzY4OGQiLCJ1c2VyX3V1aWQiOiIzZTFjMTRkZS0wMWM5LTRlYTgtYTg3Yi1mYWYyYjg1OGI1ODAifQ.zSXOUhvWKm2dNnn4ZNtV1L8K_jWPkJuCysHbMySr_QNN-6wLQpmWGffJyyYzPk0wNed4kt8QKkOSuNkJEoO6AQ"
+    Authorization: `Bearer ${CALENDLY_TOKEN}`
   }
-});
+})
 
 function calendlyapi_get_users_me_read(payload) {
-  return calendlyAPI.get(`users/me`);
+  return calendlyAPI.get(`users/me`)
 }
 
 function calendlyapi_get_scheduled_events_list(payload) {
-  return calendlyAPI.get(`scheduled_events`);
+  return calendlyAPI.get(`scheduled_events`)
 }
 
 export const apiService = {
   calendlyapi_get_users_me_read,
   calendlyapi_get_scheduled_events_list
-};
+}
