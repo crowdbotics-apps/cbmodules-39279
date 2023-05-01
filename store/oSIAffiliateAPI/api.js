a894f01f9c5cbb412666b4ab3b798d70f8ea776e
Crowdbotics <engineering@crowdbotics.com>
2023-05-01 07:20:09 +0000

    Update store.

---
+++ store/oSIAffiliateAPI/api.js
@@ -0,0 +1,23 @@
+import axios from "axios"
+const oSIAffiliateAPI = axios.create({
+  baseURL: "http://newapi.omnistarhost.com/api",
+  headers: { Accept: "application/json", "Content-Type": "application/json" }
+})
+function osiaffiliateapi_post_add_user_api_key_create(payload) {
+  return oSIAffiliateAPI.post(`/add_user/${payload.api_key}`, payload.data)
+}
+function osiaffiliateapi_post_list_users_api_key_create(payload) {
+  return oSIAffiliateAPI.post(`/list_users/${payload.api_key}`)
+}
+function osiaffiliateapi_post_delete_user_api_key_create(payload) {
+  return oSIAffiliateAPI.post(`/delete_user/${payload.api_key}`, payload.data)
+}
+function osiaffiliateapi_post_update_user_api_key_create(payload) {
+  return oSIAffiliateAPI.post(`/update_user/${payload.api_key}`, payload.data)
+}
+export const apiService = {
+  osiaffiliateapi_post_add_user_api_key_create,
+  osiaffiliateapi_post_list_users_api_key_create,
+  osiaffiliateapi_post_delete_user_api_key_create,
+  osiaffiliateapi_post_update_user_api_key_create
+}
