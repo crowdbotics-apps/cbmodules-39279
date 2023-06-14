2e648c79de0d4bd1543460085f6b93014759c2ff
Crowdbotics <engineering@crowdbotics.com>
2023-06-14 14:31:20 +0000

    Update store.

--- store/solunarDataAPI/api.js
+++
@@ -1,9 +0,0 @@
-import axios from "axios"
-const solunarDataAPI = axios.create({
-  baseURL: "https://api.solunar.org",
-  headers: { Accept: "application/json", "Content-Type": "application/json" }
-})
-function solunardataapi_get_solunar_list(payload) {
-  return solunarDataAPI.get(`solunar/`)
-}
-export const apiService = { solunardataapi_get_solunar_list }
