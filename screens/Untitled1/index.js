1899d02bda9749ef5bc1c2a85c0c263714043399
Saad bin Abid <saad.abid@crowdbotics.com>
2023-06-15 20:21:10 +0500

    [Crowdbotics Platform Commit]
    Crowdbotics Studio: Update app screens

--- screens/Untitled1/index.js
+++ screens/Untitled1/index.js
@@ -1,3 +1,11 @@
+import { openaiapis_post_v1_completions_create } from "../../store/openAIAPIs/openaiapis_response_post_Createcompletions.slice.js";
+import { openaiapis_get_v1_engines_list } from "../../store/openAIAPIs/openaiapis_response_get_Listengines.slice.js";
+import { openaiapis_post_v1_chat_completions_create } from "../../store/openAIAPIs/openaiapis_response_post_Createchatcompletions.slice.js";
+import { openaiapis_get_v1_engines_engine_id_read } from "../../store/openAIAPIs/openaiapis_response_get_Retrieveengines.slice.js";
+import { openaiapis_get_v1_models_list } from "../../store/openAIAPIs/openaiapis_response_get_Listmodels.slice.js";
+import { openaiapis_post_v1_images_generations_create } from "../../store/openAIAPIs/openaiapis_response_post_Createimages.slice.js";
+import { openaiapis_post_v1_edits_create } from "../../store/openAIAPIs/openaiapis_response_post_Createedits.slice.js";
+import { openaiapis_get_v1_models_model_read } from "../../store/openAIAPIs/openaiapis_response_get_RetrieveModels.slice.js";
 import { useEffect } from "react";
 import { useDispatch } from "react-redux";
 import React from "react";
@@ -5,7 +13,16 @@ import { StyleSheet, ScrollView, SafeAreaView } from "react-native";
 
 const Untitled1 = () => {
   const dispatch = useDispatch();
-  useEffect(() => {}, []);
+  useEffect(() => {
+    dispatch(openaiapis_get_v1_models_model_read());
+    dispatch(openaiapis_post_v1_edits_create());
+    dispatch(openaiapis_post_v1_images_generations_create());
+    dispatch(openaiapis_get_v1_models_list());
+    dispatch(openaiapis_get_v1_engines_engine_id_read());
+    dispatch(openaiapis_post_v1_chat_completions_create());
+    dispatch(openaiapis_get_v1_engines_list());
+    dispatch(openaiapis_post_v1_completions_create());
+  }, []);
   return <SafeAreaView style={styles.safeArea}>
       <ScrollView contentContainerStyle={{
       backgroundColor: "#f0f0f1",
