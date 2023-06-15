078532c4b2978554a463e022151c385129e997c9
Saad bin Abid <saad.abid@crowdbotics.com>
2023-06-15 20:14:54 +0500

    [Crowdbotics Platform Commit]
    Crowdbotics Studio: Update app screens

--- screens/Untitled1/index.js
+++ screens/Untitled1/index.js
@@ -1,11 +1,3 @@
-import { openaiconnector_get_v1_engines_read } from "../../store/openaiconnector/openaiconnector_response_get_Listengines.slice.js";
-import { openaiconnector_post_v1_images_generations_create } from "../../store/openaiconnector/openaiconnector_response_post_Createimages.slice.js";
-import { openaiconnector_post_v1_completions_create } from "../../store/openaiconnector/openaiconnector_response_post_Createcompletions.slice.js";
-import { openaiconnector_get_v1_engines_engine_id_read } from "../../store/openaiconnector/openaiconnector_response_get_Retrieveengines.slice.js";
-import { openaiconnector_get_v1_models_read } from "../../store/openaiconnector/openaiconnector_response_get_ListModels.slice.js";
-import { openaiconnector_post_v1_edits_create } from "../../store/openaiconnector/openaiconnector_response_post_Createedits.slice.js";
-import { openaiconnector_post_v1_chat_completions_create } from "../../store/openaiconnector/openaiconnector_response_post_Createchatcompletions.slice.js";
-import { openaiconnector_get_v1_models_model_read } from "../../store/openaiconnector/openaiconnector_response_get_Retrievemodels.slice.js";
 import { useEffect } from "react";
 import { useDispatch } from "react-redux";
 import React from "react";
@@ -13,16 +5,7 @@ import { StyleSheet, ScrollView, SafeAreaView } from "react-native";
 
 const Untitled1 = () => {
   const dispatch = useDispatch();
-  useEffect(() => {
-    dispatch(openaiconnector_get_v1_models_model_read());
-    dispatch(openaiconnector_post_v1_chat_completions_create());
-    dispatch(openaiconnector_post_v1_edits_create());
-    dispatch(openaiconnector_get_v1_models_read());
-    dispatch(openaiconnector_get_v1_engines_engine_id_read());
-    dispatch(openaiconnector_post_v1_completions_create());
-    dispatch(openaiconnector_post_v1_images_generations_create());
-    dispatch(openaiconnector_get_v1_engines_read());
-  }, []);
+  useEffect(() => {}, []);
   return <SafeAreaView style={styles.safeArea}>
       <ScrollView contentContainerStyle={{
       backgroundColor: "#f0f0f1",
