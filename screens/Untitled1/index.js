2cbd79cad8375801f0c53f20b442a8f57887ce4b
Saad bin Abid <saad.abid@crowdbotics.com>
2023-06-15 19:58:24 +0500

    [Crowdbotics Platform Commit]
    Crowdbotics Studio: Update app screens

--- screens/Untitled1/index.js
+++ screens/Untitled1/index.js
@@ -1,16 +1,12 @@
-import { useEffect } from "react";
-import { useDispatch } from "react-redux";
 import React from "react";
 import { StyleSheet, ScrollView, SafeAreaView } from "react-native";
 
 const Untitled1 = () => {
-  const dispatch = useDispatch();
-  useEffect(() => {}, []);
   return <SafeAreaView style={styles.safeArea}>
-        <ScrollView contentContainerStyle={{
-      backgroundColor: '#f0f0f1',
+      <ScrollView contentContainerStyle={{
+      backgroundColor: "#f0f0f1",
       padding: 10,
-      position: 'relative',
+      position: "relative",
       flex: 1
     }}></ScrollView>
     </SafeAreaView>;
@@ -18,7 +14,7 @@ const Untitled1 = () => {
 
 const styles = StyleSheet.create({
   safeArea: {
-    height: '100%'
+    height: "100%"
   }
 });
 export default Untitled1;
